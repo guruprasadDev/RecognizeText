8ea42b3ed4d798c1fcc2aff245aaf7a2a09538c6
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-02-12 13:02:34 +0530

    add initBottomSheet in MainActivity.kt

--- app/src/main/java/com/guru/recognizetext/MainActivity.kt
+++ app/src/main/java/com/guru/recognizetext/MainActivity.kt
@@ -1,33 +1,19 @@
 package com.guru.recognizetext
 
-import android.app.Activity
-import android.content.ContentValues
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.view.Menu
-import android.widget.PopupMenu
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.guru.recognizetext.databinding.ActivityMainBinding
-import com.guru.recognizetext.helperclasses.CustomDialog
 import com.guru.recognizetext.helperclasses.DialogHelper
 import com.guru.recognizetext.helperclasses.PermissionHelper
-import com.guru.recognizetext.utils.Constants.CAMERA
 import com.guru.recognizetext.utils.Constants.CAMERA_REQUEST_CODE
-import com.guru.recognizetext.utils.Constants.GALLERY
-import com.guru.recognizetext.utils.Constants.IMAGE
 import com.guru.recognizetext.utils.Constants.STORAGE_REQUEST_CODE
 import com.guru.recognizetext.utils.showToast
-import java.io.FileNotFoundException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -44,10 +30,16 @@ class MainActivity : AppCompatActivity() {
 
         dialogHelper = DialogHelper(this)
         permissionHelper = PermissionHelper(this)
-
+        initBottomSheet()
         initListener()
         initTextRecognizer()
-        initBottomSheet()
+    }
+
+    private fun initBottomSheet() {
+        imagePickerBottomSheet = ImagePickerBottomSheet(onResult = { imgUri ->
+            imageUri = imgUri // Set the value of imageUri
+            binding.imageIv.setImageURI(imgUri)
+        })
     }
 
     private fun initListener() {
@@ -64,12 +56,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initBottomSheet() {
-        imagePickerBottomSheet = ImagePickerBottomSheet(onResult = { imgUri ->
-            binding.imageIv.setImageURI(imgUri)
-        })
-    }
-
     private fun initTextRecognizer() {
         textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
     }
@@ -80,7 +66,7 @@ class MainActivity : AppCompatActivity() {
 
         try {
             val inputImage = imageUri?.let { imgUri -> InputImage.fromFilePath(this, imgUri) }
-            //  dialogHelper.alertDialog.setMessage(getString(R.string.recognize_txt))
+            dialogHelper.alertDialog.setMessage(getString(R.string.recognize_txt))
 
             inputImage?.let { image ->
                 textRecognizer.process(image)
@@ -99,46 +85,6 @@ class MainActivity : AppCompatActivity() {
             showToast("Filed to prepare timage due to ${e.message}")
         }
     }
-
-//    private fun pickImageGallery() {
-//        val intent = Intent(Intent.ACTION_PICK)
-//        intent.type = IMAGE
-//        galleryActivityResultLauncher.launch(intent)
-//    }
-//
-//    private val galleryActivityResultLauncher =
-//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-//            if (result.resultCode == Activity.RESULT_OK) {
-//                val data = result.data
-//                imageUri = data?.data
-//                binding.imageIv.setImageURI(imageUri)
-//            } else {
-//                showToast("Cancelled....")
-//            }
-//        }
-//
-//    private fun pickImageCamera() {
-//        val values = ContentValues()
-//        values.put(MediaStore.Images.Media.TITLE, getString(R.string.sample_title_message))
-//        values.put(
-//            MediaStore.Images.Media.DESCRIPTION,
-//            getString(R.string.sample_description_message)
-//        )
-//
-//        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-//        cameraActivityResultsLauncher.launch(intent)
-//    }
-//
-//    private val cameraActivityResultsLauncher =
-//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-//            if (result.resultCode == Activity.RESULT_OK) {
-//                binding.imageIv.setImageURI(imageUri)
-//            } else {
-//                showToast(getString(R.string.cancelled_message))
-//            }
-//        }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
