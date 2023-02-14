30a164d5891e062c3c1e21ee79763896201a4002
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-02-14 16:37:12 +0530

    App module class has two function provideTextRecognizer and provideImageFileManager

--- app/src/main/java/com/guru/recognizetext/di/AppModule.kt
+++ app/src/main/java/com/guru/recognizetext/di/AppModule.kt
@@ -7,15 +7,20 @@ import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.guru.recognizetext.utils.ImageFileManager
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-class AppModule(private val context: Context) {
+@InstallIn(SingletonComponent::class)
+class AppModule {
     @Provides
     @Singleton
     fun provideTextRecognizer(): TextRecognizer =
         TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
     @Provides
-    fun provideImageFileManager(): ImageFileManager = ImageFileManager(context)
+    fun provideImageFileManager(@ApplicationContext context: Context): ImageFileManager =
+        ImageFileManager(context)
 }
