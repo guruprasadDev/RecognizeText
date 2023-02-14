497c050b8412661e4a1cddb691dbfbcf8e752971
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-02-14 13:05:00 +0530

    add di in this ImagePickerBottomSheet.kt

--- app/src/main/java/com/guru/recognizetext/ImagePickerBottomSheet.kt
+++ app/src/main/java/com/guru/recognizetext/ImagePickerBottomSheet.kt
@@ -9,28 +9,31 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.guru.recognizetext.databinding.FragmentBottomSheetBinding
-import com.guru.recognizetext.helperclasses.ImageFileManager
+import com.guru.recognizetext.utils.ImageFileManager
 import com.guru.recognizetext.utils.Constants.IMAGE
+import javax.inject.Inject
 
-class ImagePickerBottomSheet(val onResult: (Uri) -> Unit) : BottomSheetDialogFragment() {
+class ImagePickerBottomSheet(
+    private val onResult: (Uri) -> Unit
+) : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentBottomSheetBinding
-    private lateinit var imageHelper: ImageFileManager
     private lateinit var galleryActivityResultLauncher: ActivityResultLauncher<String>
     private lateinit var cameraActivityResultsLauncher: ActivityResultLauncher<Uri>
 
+    @Inject
+    lateinit var imageHelper: ImageFileManager
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        (requireActivity().application as TRApplication).appComponent.initDi(this)
         galleryActivityResultLauncher =
             registerForActivityResult(ActivityResultContracts.GetContent()) { result ->
                 result?.let { uri ->
@@ -48,10 +51,7 @@ class ImagePickerBottomSheet(val onResult: (Uri) -> Unit) : BottomSheetDialogFra
             }
 
         initListener()
-        imageHelper = ImageFileManager(requireContext()).also {
-            it.createTempFile()
-            it.generateImageUri()
-        }
+        imageHelper.createTempFile()
     }
 
     private fun initListener() {
@@ -65,6 +65,7 @@ class ImagePickerBottomSheet(val onResult: (Uri) -> Unit) : BottomSheetDialogFra
             }
         }
     }
+
 
     private fun pickImageFromGallery() = galleryActivityResultLauncher.launch(IMAGE)
 
