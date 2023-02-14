84c478d506d35105abadaad1bf016c3290113a10
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-02-14 17:54:34 +0530

    image with the current system time milliseconds.

--- app/src/main/java/com/guru/recognizetext/utils/ImageFileManager.kt
+++ app/src/main/java/com/guru/recognizetext/utils/ImageFileManager.kt
@@ -7,20 +7,21 @@ import androidx.core.content.FileProvider
 import java.io.File
 import javax.inject.Inject
 
-class ImageFileManager @Inject constructor(private val context: Context ) {
+class ImageFileManager @Inject constructor(private val context: Context) {
     private lateinit var tempFile: File
-     lateinit var imageUri: Uri
+    lateinit var imageUri: Uri
 
     fun createTempFile() {
+        val uniqueValue = System.currentTimeMillis().toString()
         tempFile = File.createTempFile(
-            "image",
+            "image_$uniqueValue",
             ".jpg",
             context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         )
         generateImageUri()
     }
 
-  private fun generateImageUri() {
+    private fun generateImageUri() {
         imageUri = FileProvider.getUriForFile(
             context,
             context.packageName + ".fileprovider",
